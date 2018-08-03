ee50775dd19a9ffa0183a6412e891ad8b34d8738
Marty Chang <marty.y.chang@gmail.com>
2018-08-02 20:14:06 -0400

    Convert Metadata API source

---
+++ force-app/main/default/staticresources/SlackApiUsersInfoBarFooMock.js
@@ -0,0 +1,39 @@
+{
+    "ok": true,
+    "user": {
+        "id": "foo",
+        "team_id": "bar",
+        "name": "foo",
+        "deleted": false,
+        "color": "9f69e7",
+        "real_name": "Toh Foo",
+        "tz": "America/Los_Angeles",
+        "tz_label": "Pacific Daylight Time",
+        "tz_offset": -14400,
+        "profile": {
+            "first_name": "Toh",
+            "last_name": "Foo",
+            "avatar_hash": "g06f1a2f73d2",
+            "real_name": "Toh Foo",
+            "display_name": "foo",
+            "real_name_normalized": "Toh Foo",
+            "display_name_normalized": "foo",
+            "image_24": "https://secure.gravatar.com/avatar/06f1a2f73d247aef45630d9cc8715400.jpg?s=24&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-24.png",
+            "image_32": "https://secure.gravatar.com/avatar/06f1a2f73d247aef45630d9cc8715400.jpg?s=32&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-32.png",
+            "image_48": "https://secure.gravatar.com/avatar/06f1a2f73d247aef45630d9cc8715400.jpg?s=48&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-48.png",
+            "image_72": "https://secure.gravatar.com/avatar/06f1a2f73d247aef45630d9cc8715400.jpg?s=72&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0015-72.png",
+            "image_192": "https://secure.gravatar.com/avatar/06f1a2f73d247aef45630d9cc8715400.jpg?s=192&d=https%3A%2F%2Fa.slack-edge.com%2F7fa9%2Fimg%2Favatars%2Fava_0015-192.png",
+            "image_512": "https://secure.gravatar.com/avatar/06f1a2f73d247aef45630d9cc8715400.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2F7fa9%2Fimg%2Favatars%2Fava_0015-512.png",
+            "team": "bar"
+        },
+        "is_admin": true,
+        "is_owner": true,
+        "is_primary_owner": true,
+        "is_restricted": false,
+        "is_ultra_restricted": false,
+        "is_bot": false,
+        "updated": 1508974543,
+        "is_app_user": false,
+        "has_2fa": false
+    }
+}
