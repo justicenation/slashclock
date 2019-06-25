8ffcafb075c6ab39da61f0d9ce7a1af914efb9c8
Marty Chang <marty.y.chang@gmail.com>
2019-06-25 14:41:24 -0400

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,31 +1,36 @@
-# Check to require a username to be [given as argument][1]
-#
-# [1]: https://stackoverflow.com/questions/6482377/check-existence-of-input-argument-in-a-bash-shell-script
-if [ -z "$1" ]; then
-    echo ERROR: Username argument required!
+UNPACKAGED_DIR=.sfdx/unpackaged
 
-    # [terminate and indicate error][3]
-    #
-    # [3]: https://stackoverflow.com/questions/4381618/exit-a-script-on-error
-    exit 1
+# You can create a new file config/bash.conf which holds the following content.
+# 
+#     DEFAULT_USERNAME=slashclock-mc
+#
+# This will set the default username for this script
+LOCAL_CONF=config/bash.conf
+if [ -f "$LOCAL_CONF" ]; then
+    source "$LOCAL_CONF"
 fi
 
-MDAPI_OUTPUT_DIR=.mdapi/out
+# Prompt for the username
+while [ -z "$SFDX_USERNAME" ]
+do
+    read -p "To where shall we deploy${DEFAULT_USERNAME:+ ($DEFAULT_USERNAME)}? " SFDX_USERNAME
+    SFDX_USERNAME="${SFDX_USERNAME:-$DEFAULT_USERNAME}"
+done
 
-# Delete the converted metadata output directory [if it exists][2]
-#
-# [2]: https://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shell-script
-if [ -d "$MDAPI_OUTPUT_DIR" ]; then
-    echo found: $MDAPI_OUTPUT_DIR
-    rm -Rv $MDAPI_OUTPUT_DIR
-    echo deleted
-else
-    echo NOT found: $MDAPI_OUTPUT_DIR
+# Print a few useful pieces of information
+echo "Username: $SFDX_USERNAME"
+
+# Delete the existing deployment metadata directory if it exists
+if [ -d "$UNPACKAGED_DIR" ]; then
+    echo "Found: $UNPACKAGED_DIR"
+    echo "Deleting..."
+    rm -R "$UNPACKAGED_DIR" || exit 1
 fi
 
-mkdir -p $MDAPI_OUTPUT_DIR
+# Convert the source
+sfdx force:source:convert -d $UNPACKAGED_DIR || exit 1
 
-# Convert the source and deploy
-sfdx force:source:convert -d "$MDAPI_OUTPUT_DIR"
-sfdx force:mdapi:deploy -d "$MDAPI_OUTPUT_DIR" -w 5 -u "$1" \
-        -l "RunLocalTests"
+# Deploy the metadata
+time sfdx force:mdapi:deploy -u $SFDX_USERNAME \
+    -d $UNPACKAGED_DIR \
+    -w 5 || exit 1
